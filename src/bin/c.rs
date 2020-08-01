d45fb919a7d063cf8e2a13ef94a9e2b541450397
masa <masatoshi.sboard@gmail.com>
2020-08-01 19:22:08 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -1,8 +1,25 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
+use std::collections::HashMap;
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        a: [usize; n],
     }
+    let mut map: HashMap<usize, usize> = HashMap::new();
+    let mut ans = 0;
+    for i in a.iter() {
+        map.entry(*i).and_modify(|e| *e += 1).or_insert(1);
+        if map.get(&*i).unwrap() > i {
+            ans += 1;
+        }
+    }
+    for (i, v) in map.iter() {
+        if i > v {
+            ans += v;
+        }
+    }
+    println!("{}", ans);
 }
